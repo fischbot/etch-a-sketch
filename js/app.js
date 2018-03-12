84451d11372f6395c5bfe09898c3c346cd992e6d
robotspacefish <9095316+robotspacefish@users.noreply.github.com>
2018-03-12 11:03:29 -0400

    key up and down handlers

--- js/app.js
+++ js/app.js
@@ -14,6 +14,8 @@ window.addEventListener("load", () => {
   // draw background
   draw.rect(0, 0, canvas.width, canvas.height, "rgb(210, 210, 210)");
 
+  document.addEventListener("keydown", function(e) {game.keyHandler(e, true);}, false);
+  document.addEventListener("keyup", function(e) {game.keyHandler(e, false);}, false);
 });
 
 const draw = {
@@ -27,4 +29,33 @@ const draw = {
 
     this.rect(ink.posX, ink.posY, ink.SIZE, ink.SIZE, "rgba(0, 0, 0, 0.8)");
   }
+};
+
+const game = {
+  isUpKey : false,
+  isDownKey : false,
+  isLeftKey : false,
+  isRightKey : false,
+  leftDialAngle: 0,
+  rightDialAngle: 0,
+  keyHandler : function(e, value) {
+    var keyID = e.keyCode || e.which;
+    if (keyID === 75) {  // UP
+      this.isUpKey = value;
+      e.preventDefault();
+    }
+    if (keyID === 76) {  // DOWN
+      this.isDownKey = value;
+      e.preventDefault();
+    }
+    if (keyID === 65) {  // LEFT
+      this.isLeftKey = value;
+      e.preventDefault();
+    }
+    if (keyID === 83) {  // RIGHT
+      this.isRightKey = value;
+      e.preventDefault();
+    }
+  },
+  },
 };
